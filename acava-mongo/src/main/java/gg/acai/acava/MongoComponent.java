5eab0a9fc181dc6ace22cf5e4d2f8cf97dcb3175
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-02 22:21:23 +0100

    create `MongoComponent`

---
+++ acava-mongo/src/main/java/gg/acai/acava/MongoComponent.java
@@ -0,0 +1,74 @@
+package gg.acai.acava;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoCollection;
+import gg.acai.acava.entity.Entity;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.concurrent.CompletableFuture;
+
+/**
+ * @author Clouke
+ * @since 02.12.2022 21:24
+ * © Acava - All Rights Reserved
+ */
+public interface MongoComponent extends Closeable {
+
+    static MongoServer.Builder newBuilder() {
+        return new MongoServer.Builder();
+    }
+
+    /**
+     * Connect the component to the Mongo database
+     *
+     * @return The component
+     */
+    MongoComponent connect();
+
+    /**
+     * Gets the client of the component
+     *
+     * @return The client
+     */
+    MongoClient getClient();
+
+    /**
+     * Get a collection from the registry.
+     *
+     * @param collection the collection name
+     * @return The collection
+     */
+    MongoCollection<Document> getCollection(String collection);
+
+    /**
+     * Generate an entity from a collection, skip serialization.
+     *
+     * @param collection The collection to generate the entity from.
+     * @param filters The filters to apply to the collection.
+     * @return The entity.
+     */
+    Entity generateEntity(MongoCollection<Document> collection, Bson filters);
+
+    /**
+     * Generate an entity asynchronously.
+     *
+     * @param collection The collection to generate the entity from.
+     * @param filters The filters to apply to the collection.
+     * @return A completable future containing the entity.
+     */
+    CompletableFuture<Entity> generateEntityAsync(MongoCollection<Document> collection, Bson filters);
+
+    /**
+     * Search for a specific value in a collection.
+     *
+     * @param collection The collection to search in.
+     * @param key The key to search for.
+     * @param rawType The raw type of the value.
+     * @param <T> The type of the value.
+     * @return The value of the key.
+     */
+    <T> CompletableFuture<T> search(String collection, String key, Class<T> rawType);
+
+
+}
---
+++ acava-mongo/src/main/java/gg/acai/acava/MongoServer.java
@@ -0,0 +1,180 @@
+package gg.acai.acava;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import gg.acai.acava.entity.Entity;
+import gg.acai.acava.entity.EntityGenerator;
+import org.bson.Document;
+import org.bson.conversions.Bson;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
+
+/**
+ * @author Clouke
+ * @since 02.12.2022 21:14
+ * © Acava - All Rights Reserved
+ */
+public final class MongoServer implements MongoComponent {
+
+    private final String host;
+    private final int port;
+    private final String database;
+    private final boolean auth;
+    private final String user;
+    private final String applicationName;
+    private final String password;
+    private final String authDatabase;
+    private final boolean cacheCollections;
+    private final Map<String, MongoCollection<Document>> collections;
+
+    private MongoClient client;
+    private MongoDatabase mongoDatabase;
+
+    public MongoServer(String host, int port, String database, boolean auth, String user, String applicationName, String password, String authDatabase, boolean cacheCollections) {
+        this.host = host;
+        this.port = port;
+        this.database = database;
+        this.auth = auth;
+        this.user = user;
+        this.applicationName = applicationName;
+        this.password = password;
+        this.authDatabase = authDatabase;
+        this.cacheCollections = cacheCollections;
+        this.collections = new HashMap<>();
+    }
+
+    @Override
+    public MongoComponent connect() {
+        MongoClientSettings.Builder settings = MongoClientSettings.builder()
+                .applicationName(applicationName)
+                .applyToClusterSettings(builder -> {
+                    List<ServerAddress> addresses = Collections.singletonList(new ServerAddress(host, port));
+                    builder.hosts(addresses).build();
+                });
+
+        if (auth) {
+            settings.credential(MongoCredential.createCredential(user, authDatabase, password.toCharArray()));
+        }
+
+        this.client = MongoClients.create(settings.build());
+        this.mongoDatabase = client.getDatabase(database);
+        if (cacheCollections) {
+            this.mongoDatabase.listCollectionNames()
+                    .forEach((Consumer<String>) name -> {
+                        collections.put(name, mongoDatabase.getCollection(name));
+                    });
+        }
+        return this;
+    }
+
+    @Override
+    public MongoClient getClient() {
+        return this.client;
+    }
+
+    @Override
+    public MongoCollection<Document> getCollection(String collection) {
+        return this.collections.get(collection);
+    }
+
+    @Override
+    public <T> CompletableFuture<T> search(String collection, String key, Class<T> rawType) {
+        return CompletableFuture.supplyAsync(() -> {
+            T result = null;
+            for (Document document : mongoDatabase.getCollection(collection).find()) {
+                if (document.containsKey(key)) {
+                    result = document.get(key, rawType);
+                    break;
+                }
+            }
+
+            return result;
+        });
+    }
+
+    @Override
+    public Entity generateEntity(MongoCollection<Document> collection, Bson filters) {
+        return new EntityGenerator(collection, filters);
+    }
+
+    @Override
+    public CompletableFuture<Entity> generateEntityAsync(MongoCollection<Document> collection, Bson filters) {
+        return CompletableFuture.supplyAsync(() -> generateEntity(collection, filters));
+    }
+
+    @Override
+    public void close() {
+        this.client.close();
+    }
+
+    public static class Builder {
+        private String host;
+        private int port;
+        private String database;
+        private boolean auth;
+        private String user;
+        private String password;
+        private String authDatabase;
+        private String applicationName;
+        private boolean cacheCollections;
+
+        public Builder host(String host) {
+            this.host = host;
+            return this;
+        }
+
+        public Builder port(int port) {
+            this.port = port;
+            return this;
+        }
+
+        public Builder database(String database) {
+            this.database = database;
+            return this;
+        }
+
+        public Builder auth(boolean auth) {
+            this.auth = auth;
+            return this;
+        }
+
+        public Builder applicationName(String applicationName) {
+            this.applicationName = applicationName;
+            return this;
+        }
+
+        public Builder user(String user) {
+            this.user = user;
+            return this;
+        }
+
+        public Builder password(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder authDatabase(String authDatabase) {
+            this.authDatabase = authDatabase;
+            return this;
+        }
+
+        public Builder cacheCollections() {
+            this.cacheCollections = true;
+            return this;
+        }
+
+        public MongoServer build() {
+            return new MongoServer(host, port, database, auth, user, applicationName, password, authDatabase, cacheCollections);
+        }
+    }
+}
