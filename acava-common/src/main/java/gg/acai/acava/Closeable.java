a13c2032af5f019968d4891eadaf26a1e9a56a6a
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-02 22:20:26 +0100

    create `Closeable`

---
+++ acava-common/src/main/java/gg/acai/acava/Closeable.java
@@ -0,0 +1,13 @@
+package gg.acai.acava;
+
+/**
+ * @author Clouke
+ * @since 02.12.2022 21:24
+ * © Acava - All Rights Reserved
+ */
+@FunctionalInterface
+public interface Closeable {
+
+    void close();
+
+}
