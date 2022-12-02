92bd1fcc5acbda18bafb20a6ff163b249b725f33
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-02 22:21:12 +0100

    implementations

---
+++ acava-mongo/src/main/java/gg/acai/acava/entity/EntityType.java
@@ -0,0 +1,11 @@
+package gg.acai.acava.entity;
+
+import gg.acai.acava.annotated.Use;
+
+/**
+ * @author Clouke
+ * @since 02.12.2022 21:14
+ * © Acava - All Rights Reserved
+ */
+@Use("Implement this interface to create an entity.")
+public interface EntityType {}
