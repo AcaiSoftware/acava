cd7e704f93cb8dbce0071d2298237c521e01923e
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-03 20:01:54 +0100

    evicting `CacheDuplex<K, V>` implementation for max size of its map

---
+++ acava-common/src/main/java/gg/acai/acava/cache/CacheEvicting.java
@@ -0,0 +1,55 @@
+package gg.acai.acava.cache;
+
+import gg.acai.acava.collect.maps.EvictingMap;
+
+import java.util.Map;
+
+/**
+ * @author Clouke
+ * @since 03.12.2022 19:47
+ * © Acava - All Rights Reserved
+ */
+public class CacheEvicting<K, V> implements CacheDuplex<K, V> {
+
+    private final Map<K, V> cache;
+
+    public CacheEvicting(int maxSize) {
+        this.cache = new EvictingMap<>(maxSize);
+    }
+
+    @Override
+    public V get(K key) {
+        return this.cache.get(key);
+    }
+
+    @Override
+    public void set(K key, V value) {
+        this.cache.put(key, value);
+    }
+
+    @Override
+    public boolean exists(K key) {
+        return this.cache.containsKey(key);
+    }
+
+    @Override
+    public void delete(K key) {
+        this.cache.remove(key);
+    }
+
+    @Override
+    public int size() {
+        return this.cache.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return this.cache.isEmpty();
+    }
+
+    @Override
+    public void invalidate() {
+        this.cache.clear();
+    }
+
+}
