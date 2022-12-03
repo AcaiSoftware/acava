bd391762c2dd927112b2f6b658eb4335811b2bbc
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-03 15:04:51 +0100

    add `display` comments

--- acava-mongo/src/main/java/gg/acai/acava/entity/EntityType.java
+++ acava-mongo/src/main/java/gg/acai/acava/entity/EntityType.java
@@ -6,6 +6,30 @@ import gg.acai.acava.annotated.Use;
  * @author Clouke
  * @since 02.12.2022 21:14
  * © Acava - All Rights Reserved
+ *
+ * <pre>
+ *  {@code
+ *  public class Class implements EntityType {
+ *    private final String name;
+ *    private final UUID uniqueId;
+ *    private final int id;
+ *
+ *    public Class(EntityGenerator root) { // requires EntityGenerator as parameter
+ *        this.name = root.getValue("name", String.class, "N/A");
+ *        this.uniqueId = UUID.fromString(root.getValue("uniqueId", String.class, "00000000-0000-0000-0000-000000000000"));
+ *        this.id = root.getValue("id", Integer.class, 0);
+ *    }
+ *
+ *    public String getName() {
+ *       return name;
+ *    }
+ *
+ *    public UUID getUniqueId() {
+ *       return uniqueId;
+ *    }
+ *  }
+ *}
+ * </pre>
  */
 @Use("Implement this interface to create an entity.")
 public interface EntityType {}
