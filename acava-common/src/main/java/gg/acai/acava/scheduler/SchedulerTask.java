555afabadec75f87a15244493a09884608906ef5
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-03 12:54:50 +0100

    implementations of `SchedulerTask`

---
+++ acava-common/src/main/java/gg/acai/acava/scheduler/SchedulerTask.java
@@ -0,0 +1,52 @@
+package gg.acai.acava.scheduler;
+
+import gg.acai.acava.io.Closeable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author Clouke
+ * @since 03.12.2022 12:17
+ * © Acava - All Rights Reserved
+ */
+public interface SchedulerTask extends Closeable {
+
+    /**
+     * Schedules a task to be executed after the given delay.
+     *
+     * @param unit The unit of time.
+     * @param delay The delay in the specified unit.
+     * @return The scheduler task.
+     */
+    SchedulerTask later(TimeUnit unit, long delay);
+
+    /**
+     * Schedules a task to be executed every given interval.
+     *
+     * @param unit The unit of time.
+     * @param interval The interval in the specified unit.
+     * @return The scheduler task.
+     */
+    SchedulerTask every(TimeUnit unit, long interval);
+
+    /**
+     * Sets the action to be executed.
+     *
+     * @param action The action to be executed.
+     * @return The scheduler task.
+     */
+    SchedulerTask action(Runnable action);
+
+    /**
+     * Starts the task.
+     *
+     * @return The scheduler task.
+     */
+    SchedulerTask start();
+
+    /**
+     * Cancels the task.
+     */
+    void cancel();
+
+}
