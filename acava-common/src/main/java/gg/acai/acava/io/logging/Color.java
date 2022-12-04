ae8364d4e0cb12525408bd91714229393224e461
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-04 16:53:33 +0100

    create `Color` types for future logging purposes `(such as System.out.println(Color.BLACK + " x "`);

---
+++ acava-common/src/main/java/gg/acai/acava/io/logging/Color.java
@@ -0,0 +1,20 @@
+package gg.acai.acava.io.logging;
+
+/**
+ * @author Clouke
+ * @since 04.12.2022 14:26
+ * © Acava - All Rights Reserved
+ */
+public class Color {
+
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
+
+}
