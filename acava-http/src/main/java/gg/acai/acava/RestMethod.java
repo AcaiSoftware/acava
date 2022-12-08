b5837e942d2d95cf7f0dcf80b7e5b5e06a600da7
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-08 23:57:49 +0100

    `Rest` method specifier

---
+++ acava-http/src/main/java/gg/acai/acava/RestMethod.java
@@ -0,0 +1,17 @@
+package gg.acai.acava;
+
+/**
+ * @author Clouke
+ * @since 08.12.2022 23:22
+ * © Acava - All Rights Reserved
+ */
+public enum RestMethod {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    PATCH,
+    HEAD,
+    OPTIONS,
+    TRACE
+}
