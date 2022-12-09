5172eb42eccc177b67e34aafd7c6e20993927c8b
Clouke <76826179+Clouke@users.noreply.github.com>
2022-12-09 15:42:52 +0100

    create `Timings<T>`

---
+++ acava-common/src/main/java/gg/acai/acava/io/timing/Timings.java
@@ -0,0 +1,14 @@
+package gg.acai.acava.io.timing;
+
+/**
+ * @author Clouke
+ * @since 05.12.2022 10:29
+ * © Acava - All Rights Reserved
+ */
+public interface Timings<T> {
+
+    void begin();
+
+    void end();
+
+}
