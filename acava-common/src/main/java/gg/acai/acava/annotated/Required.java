876053d51de06b1285f3a136663f2943b80368ba
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-08 19:06:54 +0100

    optimize imports

--- acava-common/src/main/java/gg/acai/acava/annotated/Required.java
+++ acava-common/src/main/java/gg/acai/acava/annotated/Required.java
@@ -1,6 +1,9 @@
 package gg.acai.acava.annotated;
 
-import java.lang.annotation.*;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 /**
  * Marked elements are required to be present.
