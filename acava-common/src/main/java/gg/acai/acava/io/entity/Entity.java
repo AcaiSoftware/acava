eaa685a5239eab8cdfe5d51cfcae7622212e23aa
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-08 19:07:36 +0100

    `Entity` marker interface for implementations

---
+++ acava-common/src/main/java/gg/acai/acava/io/entity/Entity.java
@@ -0,0 +1,11 @@
+package gg.acai.acava.io.entity;
+
+import gg.acai.acava.annotated.Required;
+
+/**
+ * @author Clouke
+ * @since 08.02.2023 15:50
+ * © Acava - All Rights Reserved
+ */
+@Required
+public interface Entity {}
