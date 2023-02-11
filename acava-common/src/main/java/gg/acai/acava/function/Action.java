365bef2ebb7d879e083f9909e9dbe2a243ba667a
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-11 14:20:11 +0100

    add docs

--- acava-common/src/main/java/gg/acai/acava/function/Action.java
+++ acava-common/src/main/java/gg/acai/acava/function/Action.java
@@ -1,6 +1,8 @@
 package gg.acai.acava.function;
 
 /**
+ * An action is a function that takes a single argument and returns no result.
+ *
  * @author Clouke
  * @since 02.12.2022 18:08
  * © Acava - All Rights Reserved
@@ -8,8 +10,19 @@ package gg.acai.acava.function;
 @FunctionalInterface
 public interface Action<T> {
 
+    /**
+     * Performs this operation on the given argument.
+     *
+     * @param t the input argument
+     */
     void accept(T t);
 
+    /**
+     * Returns a composed action that performs, in sequence, this action followed by the after action.
+     *
+     * @param after the action to perform after this action
+     * @return a composed action that performs in sequence this action followed by the after action
+     */
     default Action<T> andThen(Action<? super T> after) {
         return (T t) -> {
             accept(t);
