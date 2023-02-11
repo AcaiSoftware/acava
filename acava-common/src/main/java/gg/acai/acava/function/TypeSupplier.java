80eb40c2a55f8120071aecb2279c2296f3dbc811
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-11 14:20:36 +0100

    add docs

--- acava-common/src/main/java/gg/acai/acava/function/TypeSupplier.java
+++ acava-common/src/main/java/gg/acai/acava/function/TypeSupplier.java
@@ -1,6 +1,8 @@
 package gg.acai.acava.function;
 
 /**
+ * A type supplier is a functional interface that can be used to get the value of a type.
+ *
  * @author Clouke
  * @since 02.12.2022 18:10
  * © Acava - All Rights Reserved
@@ -8,13 +10,32 @@ package gg.acai.acava.function;
 @FunctionalInterface
 public interface TypeSupplier<T, U> {
 
+    /**
+     * Gets the value of the type
+     *
+     * @param u the type
+     * @return the value of the type
+     */
     T get(U u);
 
+    /**
+     * Gets the value of the type, or the default value if the type is null.
+     *
+     * @param u the type
+     * @param t the default value
+     * @return the value of the type, or the default value if the type is null.
+     */
     default T getOrDefault(U u, T t) {
         T value = get(u);
         return value == null ? t : value;
     }
 
+    /**
+     * Computes the value of the type through {@link Action<U>}
+     *
+     * @param u the type
+     * @param action the action to compute the value
+     */
     default void getIfPresent(U u, Action<T> action) {
         T value = get(u);
         if (value != null) action.accept(value);
