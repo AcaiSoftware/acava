d0de87590e8b69a04b54151639bd1c88a9730459
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-11 17:51:03 +0100

    update docs

--- acava-common/src/main/java/gg/acai/acava/collect/meta/MetadataMapper.java
+++ acava-common/src/main/java/gg/acai/acava/collect/meta/MetadataMapper.java
@@ -4,20 +4,29 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
+ * MetadataMapper - A mapper extension for metadata
+ *
  * @author Clouke
  * @since 02.12.2022 18:17
  * © Acava - All Rights Reserved
- *
- * MetadataMapper - A mapper extension for metadata
  */
 public class MetadataMapper<K, V> {
 
+    /**
+     * The map that holds the metadata
+     */
     private final Map<K, V> map;
 
+    /**
+     * @param map The map to use as the metadata
+     */
     public MetadataMapper(Map<K, V> map) {
         this.map = map;
     }
 
+    /**
+     * Creates a new MetadataMapper with a new HashMap
+     */
     public MetadataMapper() {
         this.map = new HashMap<>();
     }
@@ -29,10 +38,18 @@ public class MetadataMapper<K, V> {
         this.map.put(entry.key(), entry.value());
     }
 
+    /**
+     * @param key The key to add to the map
+     * @param value The value to add to the map
+     */
     public void add(K key, V value) {
         this.map.put(key, value);
     }
 
+    /**
+     * @param key The key to get from the map
+     * @return Returns the value of the key
+     */
     public MetadataEntry<K, V> get(K key) {
         return new MetadataEntry<K, V>() {
             @Override
@@ -47,10 +64,17 @@ public class MetadataMapper<K, V> {
         };
     }
 
+    /**
+     * @param key The key to remove from the map
+     * @return Returns the value of the key
+     */
     public V remove(K key) {
         return this.map.remove(key);
     }
 
+    /**
+     * @return Returns the map
+     */
     public Map<K, V> asMap() {
         return this.map;
     }
