63a79ec1cd808f0420b6aafe7a0763f74754b40d
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-14 04:25:26 +0100

    add docs

--- acava-common/src/main/java/gg/acai/acava/io/Filter.java
+++ acava-common/src/main/java/gg/acai/acava/io/Filter.java
@@ -4,14 +4,26 @@ import java.util.Set;
 import java.util.function.Predicate;
 
 /**
+ * A simple filter extension of {@link Predicate}.
+ *
  * @author Clouke
  * @since 04.12.2022 23:13
  * © Acava - All Rights Reserved
  */
 public interface Filter<T> extends Predicate<T> {
 
+    /**
+     * Adds the given object to the filter.
+     *
+     * @param t Object to add
+     */
     void add(T t);
 
+    /**
+     * Gets the set of objects that have been seen.
+     *
+     * @return Set of objects that have been seen
+     */
     Set<T> getSeen();
 
 }
