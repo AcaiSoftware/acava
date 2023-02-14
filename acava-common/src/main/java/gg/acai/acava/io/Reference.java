1cda274603e9e60df694c908c3de58f5c7a69240
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-14 04:25:39 +0100

    add docs

--- acava-common/src/main/java/gg/acai/acava/io/Reference.java
+++ acava-common/src/main/java/gg/acai/acava/io/Reference.java
@@ -1,22 +1,42 @@
 package gg.acai.acava.io;
 
 /**
+ * A reference object that can be used to pass a value by reference.
+ *
  * @author Clouke
  * @since 05.12.2022 18:58
  * © Acava - All Rights Reserved
  */
 public final class Reference<V> {
 
+    /**
+     * The value of the reference.
+     */
     private V value;
 
+    /**
+     * Creates a new reference with the given value.
+     *
+     * @param value The value of the reference
+     */
     public Reference(V value) {
         this.value = value;
     }
 
+    /**
+     * Gets the value of the reference.
+     *
+     * @return The value of the reference
+     */
     public V get() {
         return this.value;
     }
 
+    /**
+     * Sets the value of the reference.
+     *
+     * @param value The value of the reference
+     */
     public void set(V value) {
         this.value = value;
     }
