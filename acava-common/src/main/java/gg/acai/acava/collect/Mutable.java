8b41459bd7b7de35dc0fb64fc6fcd0fed7b90039
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-23 02:18:22 +0100

    create `Mutable` marker

---
+++ acava-common/src/main/java/gg/acai/acava/collect/Mutable.java
@@ -0,0 +1,10 @@
+package gg.acai.acava.collect;
+
+/**
+ * Marker interface for mutable objects.
+ *
+ * @author Clouke
+ * @since 22.02.2023 21:21
+ * © Acava - All Rights Reserved
+ */
+public interface Mutable {}
