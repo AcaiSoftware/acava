1d984f59f79cea0bdefe530acd3b0785359c99d7
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-23 06:08:14 +0100

    add docs

--- acava-common/src/main/java/gg/acai/acava/commons/graph/GraphVisualizer.java
+++ acava-common/src/main/java/gg/acai/acava/commons/graph/GraphVisualizer.java
@@ -1,21 +1,32 @@
 package gg.acai.acava.commons.graph;
 
+import gg.acai.acava.scheduler.AsyncPlaceholder;
+import gg.acai.acava.scheduler.Schedulers;
+
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
 
 /**
+ * A utility class for visualizing graphs.
+ * @param <N> a type parameter that extends Number
+ *
  * @author Clouke
  * @since 23.02.2023 00:36
  * © Acava - All Rights Reserved
  */
 public final class GraphVisualizer<N extends Number> {
 
+    // Constants for creating the visualization
     private static final char FULL_BLOCK = '█';
     private static final char DARK_SHADE = '▓';
     private static final char MEDIUM_SHADE = '▒';
     private static final char LIGHT_SHADE = '░';
 
+    /**
+     * A function that takes an AbstractGraph object and returns a string
+     * representing the visualization of the graph.
+     */
     private static final Function<AbstractGraph<? extends Number>, String> VISUALIZER = (graph) -> {
         int[] data = graph.nodes.stream()
                 .mapToInt(Number::intValue)
@@ -51,14 +62,39 @@ public final class GraphVisualizer<N extends Number> {
 
     private final AbstractGraph<N> graph;
 
+    /**
+     * Constructs a new GraphVisualizer with the specified graph.
+     *
+     * @param graph the graph to visualize
+     */
     public GraphVisualizer(AbstractGraph<N> graph) {
         this.graph = graph;
     }
 
+    /**
+     * Returns a string representing the visualization of the graph.
+     *
+     * @return a string representing the visualization of the graph
+     */
     public String visualize() {
         return VISUALIZER.apply(graph);
     }
 
+    /**
+     * Returns an AsyncPlaceholder representing the asynchronous computation
+     * of the visualize method.
+     *
+     * @return an AsyncPlaceholder representing the asynchronous computation of the visualize method
+     */
+    public AsyncPlaceholder<String> visualizeAsync() {
+        return Schedulers.supplyAsync(this::visualize);
+    }
+
+    /**
+     * Returns a simple string representation of the graph.
+     *
+     * @return a simple string representation of the graph
+     */
     public String simpleVisualization() {
         StringBuilder builder = new StringBuilder();
         List<N> nodes = graph.nodes;
@@ -71,10 +107,43 @@ public final class GraphVisualizer<N extends Number> {
         return builder.toString();
     }
 
+    /**
+     * Returns an AsyncPlaceholder representing the asynchronous computation
+     * of the simpleVisualization() method.
+     *
+     * @return an AsyncPlaceholder representing the asynchronous computation
+     * of the simpleVisualization() method
+     */
+    public AsyncPlaceholder<String> simpleVisualizationAsync() {
+        return Schedulers.supplyAsync(this::simpleVisualization);
+    }
+
+    /**
+     * Prints the visualization of the graph to the standard output.
+     */
     public void print() {
         System.out.println(visualize());
     }
 
+    /**
+     * Returns an AsyncPlaceholder representing the asynchronous computation
+     * of the print() method.
+     *
+     * @return an AsyncPlaceholder representing the asynchronous computation
+     * of the print() method
+     */
+    public AsyncPlaceholder<GraphVisualizer<N>> printAsync() {
+        return Schedulers.supplyAsync(() -> {
+            print();
+            return this;
+        });
+    }
+
+    /**
+     * Returns a string representing the visualization of the graph.
+     *
+     * @return a string representing the visualization of the graph
+     */
     @Override
     public String toString() {
         return visualize();
