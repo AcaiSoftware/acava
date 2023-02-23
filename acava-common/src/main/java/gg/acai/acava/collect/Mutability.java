3454ad854b9c4f15926a9977f4e1541b5e306838
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-23 02:19:34 +0100

    create `Mutability` context

---
+++ acava-common/src/main/java/gg/acai/acava/collect/Mutability.java
@@ -0,0 +1,20 @@
+package gg.acai.acava.collect;
+
+/**
+ * A simple enum to determine if a collection is mutable or immutable.
+ *
+ * @author Clouke
+ * @since 22.02.2023 07:42
+ * © Acava - All Rights Reserved
+ */
+public enum Mutability {
+    /**
+     * The collection is mutable.
+     */
+    MUTABLE,
+
+    /**
+     * The collection is immutable.
+     */
+    IMMUTABLE
+}
