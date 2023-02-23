00d889cb324688d7fdc63ecc3e6e665a3c42b77a
Clouke <76826179+Clouke@users.noreply.github.com>
2023-02-23 02:14:56 +0100

    create `FixedSizeGraph<N>`, implementation of `Graph<N>`, `AbstractGraph<N>`

---
+++ acava-common/src/main/java/gg/acai/acava/commons/graph/FixedSizeGraph.java
@@ -0,0 +1,72 @@
+package gg.acai.acava.commons.graph;
+
+import gg.acai.acava.annotated.Use;
+import gg.acai.acava.collect.Mutable;
+
+import java.util.Collection;
+import java.util.Collections;
+
+/**
+ * @author Clouke
+ * @since 22.02.2023 22:16
+ * © Acava - All Rights Reserved
+ */
+@Use("Use Graph#newBuilder() to create a new graph")
+public class FixedSizeGraph<N extends Number> extends AbstractGraph<N> implements Graph<N>, Mutable {
+
+    public FixedSizeGraph(int size, int height, int limiter) {
+        super(size, height, limiter);
+    }
+
+    @Override
+    public Graph<N> addNode(N node) {
+        super.nodes.add(node);
+        return this;
+    }
+
+    @Override
+    public Graph<N> addNodes(Collection<N> nodes) {
+        super.nodes.addAll(nodes);
+        return this;
+    }
+
+    @Override @SafeVarargs
+    public final Graph<N> addNodes(N... nodes) {
+        Collections.addAll(super.nodes, nodes);
+        return this;
+    }
+
+    @Override
+    public Graph<N> delegate(Graph<N> other) {
+        int nodes = other.getNodes();
+        for (int i = 0; i < nodes; i++) {
+            super.nodes.add(other.getNode(i));
+        }
+        return this;
+    }
+
+    @Override
+    public GraphVisualizer<N> getVisualizer() {
+        return super.visualizer;
+    }
+
+    @Override
+    public int getHeight() {
+        return height;
+    }
+
+    @Override
+    public int getLimiter() {
+        return limiter;
+    }
+
+    @Override
+    public N getNode(int index) {
+        return super.nodes.get(index);
+    }
+
+    @Override
+    public int getNodes() {
+        return super.nodes.size();
+    }
+}
