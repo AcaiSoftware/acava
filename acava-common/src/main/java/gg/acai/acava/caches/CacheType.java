aeb0772d77183f7fa6815c223860506e71e7b6cf
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-19 18:35:01 +0100

    create `CacheType` - recoding `cache`

---
+++ acava-common/src/main/java/gg/acai/acava/caches/CacheType.java
@@ -0,0 +1,12 @@
+package gg.acai.acava.caches;
+
+/**
+ * @author Clouke
+ * @since 19.03.2023 15:07
+ * © Acava - All Rights Reserved
+ */
+public enum CacheType {
+  FIXED_SIZE,
+  LRU,
+  DEFAULT
+}
