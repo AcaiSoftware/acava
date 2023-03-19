9377f6badd058bc395d44e878af74e043aff9389
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-19 22:40:04 +0100

    refactor `CacheNode<V>`

--- acava-common/src/main/java/gg/acai/acava/caches/element/DefaultCacheNode.java
+++ acava-common/src/main/java/gg/acai/acava/caches/element/DefaultCacheNode.java
@@ -1,17 +1,17 @@
 package gg.acai.acava.caches.element;
 
-import gg.acai.acava.caches.CacheElement;
+import gg.acai.acava.caches.CacheNode;
 
 /**
  * @author Clouke
  * @since 19.03.2023 15:21
  * © Acava - All Rights Reserved
  */
-public final class DefaultCacheElement<V> implements CacheElement<V> {
+public final class DefaultCacheNode<V> implements CacheNode<V> {
 
   private V value;
 
-  public DefaultCacheElement(V value) {
+  public DefaultCacheNode(V value) {
     this.value = value;
   }
 
