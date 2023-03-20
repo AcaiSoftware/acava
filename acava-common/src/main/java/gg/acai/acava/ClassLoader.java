2bcc640e1369e80185da22b47237dd78a31a88b7
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 19:28:28 +0100

    minor tweaks

--- acava-common/src/main/java/gg/acai/acava/ClassLoader.java
+++ acava-common/src/main/java/gg/acai/acava/ClassLoader.java
@@ -1,61 +1,62 @@
 package gg.acai.acava;
 
+import gg.acai.acava.collect.lists.ImmutableHashSet;
+
 import java.io.IOException;
 import java.net.URL;
 import java.security.CodeSource;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Enumeration;
-import java.util.HashSet;
 import java.util.jar.JarEntry;
 import java.util.jar.JarFile;
 
 public final class ClassLoader {
 
-    public static Collection<Class<?>> getClasses(Class<?> main, String packageName) {
-        JarFile jarFile;
-        Collection<Class<?>> classes = new ArrayList<>();
-        CodeSource codeSource = main.getProtectionDomain().getCodeSource();
-        URL resource = codeSource.getLocation();
+  public static Collection<Class<?>> getClasses(Class<?> main, String packageName) {
+    JarFile jarFile;
+    Collection<Class<?>> classes = new ArrayList<>();
+    CodeSource codeSource = main.getProtectionDomain().getCodeSource();
+    URL resource = codeSource.getLocation();
 
-        String relPath = packageName.replace('.', '/');
-        String resPath = resource.getPath().replace("%20", " ");
-        String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
+    String relPath = packageName.replace('.', '/');
+    String resPath = resource.getPath().replace("%20", " ");
+    String jarPath = resPath.replaceFirst("[.]jar[!].*", ".jar").replaceFirst("file:", "");
 
-        try {
-            jarFile = new JarFile(jarPath);
-        } catch (IOException e) {
-            throw new IllegalStateException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
-        }
-
-        Enumeration<JarEntry> entries = jarFile.entries();
-        while (entries.hasMoreElements()) {
-            JarEntry entry = entries.nextElement();
-            String entryName = entry.getName();
-            String className = null;
-            if (entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > relPath.length() + "/".length()) {
-                className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
-            }
-            if (className != null) {
-                Class<?> clazz = null;
-                try {
-                    clazz = Class.forName(className);
-                } catch (ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-                if (clazz != null) {
-                    classes.add(clazz);
-                }
-            }
-        }
-
-        try {
-            jarFile.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        return new HashSet<>(classes);
+    try {
+      jarFile = new JarFile(jarPath);
+    } catch (IOException e) {
+      throw new IllegalStateException("Unexpected IOException reading JAR File '" + jarPath + "'", e);
     }
+
+    Enumeration<JarEntry> entries = jarFile.entries();
+    while (entries.hasMoreElements()) {
+      JarEntry entry = entries.nextElement();
+      String entryName = entry.getName();
+      String className = null;
+      if (entryName.endsWith(".class") && entryName.startsWith(relPath) && entryName.length() > relPath.length() + "/".length()) {
+        className = entryName.replace('/', '.').replace('\\', '.').replace(".class", "");
+      }
+      if (className != null) {
+        Class<?> clazz = null;
+        try {
+          clazz = Class.forName(className);
+        } catch (ClassNotFoundException e) {
+          e.printStackTrace();
+        }
+        if (clazz != null) {
+          classes.add(clazz);
+        }
+      }
+    }
+
+    try {
+      jarFile.close();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+    return new ImmutableHashSet<>(classes);
+  }
 
 }
