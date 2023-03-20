bf5a1fadb45db2d1a37b1a0c13a7c435c72f3e5d
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 01:26:16 +0100

    create `Cache<K,V>` implementation `LazyWriteExpiryCache<K,V>`

---
+++ acava-common/src/main/java/gg/acai/acava/caches/LazyWriteExpiryCache.java
@@ -0,0 +1,65 @@
+package gg.acai.acava.caches;
+
+import gg.acai.acava.Requisites;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Clouke
+ * @since 19.03.2023 22:32
+ * © Acava - All Rights Reserved
+ */
+public final class LazyWriteExpiryCache<K, V> extends AbstractCache<K, V> {
+
+  private final Map<K, Long> writes = new HashMap<>();
+
+  @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+  public LazyWriteExpiryCache(CacheType type, Optional<Integer> size, long expireAfterWrite, TimeUnit unit, CacheBootstrap bootstrap, ParametricCacheBootstrap<?> pcb, CacheReferenceType cacheReferenceType, Lock lock) {
+    super(type, size, expireAfterWrite, unit, bootstrap, pcb, cacheReferenceType, lock);
+    Requisites.checkArgument(expireAfterWrite > 0, "Expire after write must be greater than 0");
+  }
+
+  @Override
+  public void put(K key, V value) {
+    try {
+      lock();
+      writes.forEach((k, v) -> {
+        long now = System.currentTimeMillis();
+        if (now - v >= unit.toMillis(expireAfterWrite)) {
+          synchronized (writes) {
+            cache.remove(k);
+            writes.remove(k);
+          }
+        }
+      });
+      writes.put(key, System.currentTimeMillis());
+      super.put(key, value);
+    } finally {
+      unlock();
+    }
+  }
+
+  @Override
+  public Optional<V> get(K key) {
+    try {
+      lock();
+      writes.forEach((k, v) -> {
+        long now = System.currentTimeMillis();
+        if (now - v >= unit.toMillis(expireAfterWrite)) {
+          synchronized (writes) {
+            cache.remove(k);
+            writes.remove(k);
+          }
+        }
+      });
+      return super.get(key);
+    } finally {
+      unlock();
+    }
+  }
+
+}
