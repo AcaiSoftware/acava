1667a9d33ab0834bc8000ca5884ca186f3486282
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 18:24:14 +0100

    create `MutableChart`

---
+++ acava-common/src/main/java/gg/acai/acava/commons/chart/MutableChart.java
@@ -0,0 +1,15 @@
+package gg.acai.acava.commons.chart;
+
+import gg.acai.acava.collect.Mutable;
+
+/**
+ * @author Clouke
+ * @since 03.03.2023 22:03
+ * © Acava - All Rights Reserved
+ */
+public class MutableChart extends AbstractChart implements Mutable {
+
+    public MutableChart(ChartBuilder builder) {
+        super(builder);
+    }
+}
