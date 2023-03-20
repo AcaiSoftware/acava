b31ccaa1faf285e1a8b01fdc99fb8ae4c0206e48
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 01:25:04 +0100

    create `ParametricCacheBootstrap`

---
+++ acava-common/src/main/java/gg/acai/acava/caches/ParametricCacheBootstrap.java
@@ -0,0 +1,17 @@
+package gg.acai.acava.caches;
+
+import gg.acai.acava.io.Closeable;
+
+/**
+ * @author Clouke
+ * @since 23.02.2023 16:03
+ * © Acava - All Rights Reserved
+ */
+public interface ParametricCacheBootstrap<T> extends Closeable {
+
+  void load(T t);
+
+  @Override
+  void close();
+
+}
