99965bf8436b93ce4eada6b5d09916eba9dc529e
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 19:28:19 +0100

    minor tweaks

--- acava-common/src/main/java/gg/acai/acava/Requisites.java
+++ acava-common/src/main/java/gg/acai/acava/Requisites.java
@@ -9,52 +9,52 @@ import java.util.function.Supplier;
  */
 public final class Requisites {
 
-    public static <T> T requireNonNull(T object) {
-        if (object == null) {
-            throw new NullPointerException();
-        }
-        return object;
+  public static <T> T requireNonNull(T object) {
+    if (object == null) {
+      throw new NullPointerException();
     }
+    return object;
+  }
 
-    public static <T> T requireNonNull(T object, String message) {
-        if (object == null) {
-            throw new NullPointerException(message);
-        }
-        return object;
+  public static <T> T requireNonNull(T object, String message) {
+    if (object == null) {
+      throw new NullPointerException(message);
     }
+    return object;
+  }
 
-    public static <T> T applyIfNull(T value, T newValue) {
-        return value == null ? newValue : value;
-    }
-    public static <T> T applyIfNull(T value, Supplier<T> newValue) {
-        return value == null ? newValue.get() : value;
-    }
+  public static <T> T applyIfNull(T value, T newValue) {
+    return value == null ? newValue : value;
+  }
 
+  public static <T> T applyIfNull(T value, Supplier<T> newValue) {
+    return value == null ? newValue.get() : value;
+  }
 
-    public static <T> T applyIfNull(T value, T newValue, String message) {
-        if (value == null && newValue == null) {
-            throw new NullPointerException(message);
-        }
-        return value == null ? newValue : value;
+  public static <T> T applyIfNull(T value, T newValue, String message) {
+    if (value == null && newValue == null) {
+      throw new NullPointerException(message);
     }
+    return value == null ? newValue : value;
+  }
 
-    public static void checkArgument(boolean expression, String message) {
-        if (!expression) {
-            throw new IllegalArgumentException(message);
-        }
+  public static void checkArgument(boolean expression, String message) {
+    if (!expression) {
+      throw new IllegalArgumentException(message);
     }
+  }
 
-    public static void checkArgument(boolean expression) {
-        checkArgument(expression, "Illegal argument");
-    }
+  public static void checkArgument(boolean expression) {
+    checkArgument(expression, "Illegal argument");
+  }
 
-    public static <T> T createInstance(Class<T> clazz) {
-        try {
-            return clazz.newInstance();
-        } catch (InstantiationException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-        throw new RuntimeException("Could not create instance of " + clazz.getName());
+  public static <T> T createInstance(Class<T> clazz) {
+    try {
+      return clazz.newInstance();
+    } catch (InstantiationException | IllegalAccessException e) {
+      e.printStackTrace();
     }
+    throw new RuntimeException("Could not create instance of " + clazz.getName());
+  }
 
 }
