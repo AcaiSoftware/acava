fcfcbaa41e11223eb0526e31553c9b67effd3ddf
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 01:26:04 +0100

    create `Cache<K,V>` implementation `RemovalAfterReadCache<K,V>`

---
+++ acava-common/src/main/java/gg/acai/acava/caches/RemovalAfterReadCache.java
@@ -0,0 +1,33 @@
+package gg.acai.acava.caches;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * @author Clouke
+ * @since 20.03.2023 01:15
+ * © Acava - All Rights Reserved
+ */
+public class RemovalAfterReadCache<K, V> extends AbstractCache<K, V> {
+
+  @SuppressWarnings("OptionalUsedAsFieldOrParameterType")
+  public RemovalAfterReadCache(CacheType type, Optional<Integer> size, long expireAfterWrite, TimeUnit unit, CacheBootstrap bootstrap, ParametricCacheBootstrap<?> pcb, CacheReferenceType cacheReferenceType, Lock lock) {
+    super(type, size, expireAfterWrite, unit, bootstrap, pcb, cacheReferenceType, lock);
+  }
+
+  @Override
+  public Optional<V> get(K key) {
+    try {
+      lock();
+      Optional<V> value = super.get(key);
+      return value.map(
+        toReturn -> {
+          cache.remove(key);
+          return toReturn;
+        });
+    } finally {
+      unlock();
+    }
+  }
+}
