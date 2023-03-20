1237ab8a7e7181d3fe2723dbb2a9e665084f0d78
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-20 02:16:20 +0100

    `CacheContext` providing information for updates within the cache

---
+++ acava-common/src/main/java/gg/acai/acava/caches/CacheContext.java
@@ -0,0 +1,13 @@
+package gg.acai.acava.caches;
+
+/**
+ * @author Clouke
+ * @since 20.03.2023 02:05
+ * © Acava - All Rights Reserved
+ */
+public enum CacheContext {
+  PUT,
+  GET,
+  REMOVE,
+  INVALIDATE
+}
