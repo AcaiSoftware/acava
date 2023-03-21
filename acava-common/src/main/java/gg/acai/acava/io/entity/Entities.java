40ff5b06a4a47bba2b42a8aed18eb4c90a7b195e
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-21 02:14:18 +0100

    version `1.0.1`, change indent tabs to `2`

--- acava-common/src/main/java/gg/acai/acava/io/entity/Entities.java
+++ acava-common/src/main/java/gg/acai/acava/io/entity/Entities.java
@@ -28,97 +28,97 @@ import javax.annotation.Nullable;
  */
 public final class Entities implements Closeable {
 
-    private final HashBasedTable<Class<? extends Entity>, String, Entity> entities;
+  private final HashBasedTable<Class<? extends Entity>, String, Entity> entities;
 
-    public static Entities create() {
-        return new Entities();
-    }
+  public static Entities create() {
+    return new Entities();
+  }
 
-    public Entities() {
-        this.entities = HashBasedTable.create();
-    }
+  public Entities() {
+    this.entities = HashBasedTable.create();
+  }
 
-    /**
-     * Gets an entity from the table asynchronously.
-     *
-     * @param type The type of entity.
-     * @param identifier The identifier of the entity.
-     * @return Returns a completed {@link AsyncPlaceholder<R>} of the entity. The entity will be null if it does not exist.
-     */
-    @SuppressWarnings("unchecked")
-    public <R extends Entity> AsyncPlaceholder<R> get(@Required Class<R> type, String identifier) {
-        synchronized (this.entities) {
-            return Schedulers.supplyAsync(() -> {
-                if (this.entities.contains(type, identifier)) {
-                    return (R) this.entities.get(type, identifier);
-                }
-                return null;
-            });
+  /**
+   * Gets an entity from the table asynchronously.
+   *
+   * @param type       The type of entity.
+   * @param identifier The identifier of the entity.
+   * @return Returns a completed {@link AsyncPlaceholder<R>} of the entity. The entity will be null if it does not exist.
+   */
+  @SuppressWarnings("unchecked")
+  public <R extends Entity> AsyncPlaceholder<R> get(@Required Class<R> type, String identifier) {
+    synchronized (this.entities) {
+      return Schedulers.supplyAsync(() -> {
+        if (this.entities.contains(type, identifier)) {
+          return (R) this.entities.get(type, identifier);
         }
+        return null;
+      });
     }
+  }
 
-    /**
-     * Gets an entity from the table synchronously.
-     *
-     * @param type The type of entity.
-     * @param identifier The identifier of the entity.
-     * @return Returns the entity. The entity will be null if it does not exist.
-     */
-    @SuppressWarnings("unchecked")
-    public <R extends Entity> R getSync(Class<R> type, String identifier) {
-        synchronized (this.entities) {
-            if (this.entities.contains(type, identifier)) {
-                return (R) this.entities.get(type, identifier);
-            }
-            return null;
-        }
+  /**
+   * Gets an entity from the table synchronously.
+   *
+   * @param type       The type of entity.
+   * @param identifier The identifier of the entity.
+   * @return Returns the entity. The entity will be null if it does not exist.
+   */
+  @SuppressWarnings("unchecked")
+  public <R extends Entity> R getSync(Class<R> type, String identifier) {
+    synchronized (this.entities) {
+      if (this.entities.contains(type, identifier)) {
+        return (R) this.entities.get(type, identifier);
+      }
+      return null;
     }
+  }
 
-    /**
-     * Checks if the table contains an entity.
-     *
-     * @param type The type of entity.
-     * @param identifier The identifier of the entity.
-     * @return Returns true if the table contains the entity.
-     */
-    public boolean contains(Class<? extends Entity> type, String identifier) {
-        synchronized (this.entities) {
-            return this.entities.contains(type, identifier);
-        }
+  /**
+   * Checks if the table contains an entity.
+   *
+   * @param type       The type of entity.
+   * @param identifier The identifier of the entity.
+   * @return Returns true if the table contains the entity.
+   */
+  public boolean contains(Class<? extends Entity> type, String identifier) {
+    synchronized (this.entities) {
+      return this.entities.contains(type, identifier);
     }
+  }
 
-    /**
-     * Registers an entity to the table.
-     *
-     * @param entity The entity to register.
-     * @param id The identifier of the entity.
-     * @return Returns the registered entity.
-     */
-    public Entity register(Entity entity, String id) {
-        synchronized (this.entities) {
-            return this.entities.put(entity.getClass(), id, entity);
-        }
+  /**
+   * Registers an entity to the table.
+   *
+   * @param entity The entity to register.
+   * @param id     The identifier of the entity.
+   * @return Returns the registered entity.
+   */
+  public Entity register(Entity entity, String id) {
+    synchronized (this.entities) {
+      return this.entities.put(entity.getClass(), id, entity);
     }
+  }
 
-    /**
-     * Unregisters the entity from the table.
-     *
-     * @param entity The entity to unregister.
-     * @param id The identifier of the entity.
-     * @return Returns the entity that was removed, or null if it does not exist.
-     */
-    @Nullable
-    public Entity remove(Entity entity, String id) {
-        synchronized (this.entities) {
-            return this.entities.remove(entity.getClass(), id);
-        }
+  /**
+   * Unregisters the entity from the table.
+   *
+   * @param entity The entity to unregister.
+   * @param id     The identifier of the entity.
+   * @return Returns the entity that was removed, or null if it does not exist.
+   */
+  @Nullable
+  public Entity remove(Entity entity, String id) {
+    synchronized (this.entities) {
+      return this.entities.remove(entity.getClass(), id);
     }
+  }
 
-    /**
-     * Closes the entities table.
-     */
-    @Override
-    public void close() {
-        this.entities.clear();
-    }
+  /**
+   * Closes the entities table.
+   */
+  @Override
+  public void close() {
+    this.entities.clear();
+  }
 }
