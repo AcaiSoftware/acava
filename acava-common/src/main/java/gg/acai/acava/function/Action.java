b2deb4da5030fe821ad151cd9d4dc47080c36b03
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-21 02:13:45 +0100

    version `1.0.1`, change indent tabs to `2`

--- acava-common/src/main/java/gg/acai/acava/function/Action.java
+++ acava-common/src/main/java/gg/acai/acava/function/Action.java
@@ -10,24 +10,24 @@ package gg.acai.acava.function;
 @FunctionalInterface
 public interface Action<T> {
 
-    /**
-     * Performs this operation on the given argument.
-     *
-     * @param t the input argument
-     */
-    void accept(T t);
+  /**
+   * Performs this operation on the given argument.
+   *
+   * @param t the input argument
+   */
+  void accept(T t);
 
-    /**
-     * Returns a composed action that performs, in sequence, this action followed by the after action.
-     *
-     * @param after the action to perform after this action
-     * @return a composed action that performs in sequence this action followed by the after action
-     */
-    default Action<T> andThen(Action<? super T> after) {
-        return (T t) -> {
-            accept(t);
-            after.accept(t);
-        };
-    }
+  /**
+   * Returns a composed action that performs, in sequence, this action followed by the after action.
+   *
+   * @param after the action to perform after this action
+   * @return a composed action that performs in sequence this action followed by the after action
+   */
+  default Action<T> andThen(Action<? super T> after) {
+    return (T t) -> {
+      accept(t);
+      after.accept(t);
+    };
+  }
 
 }
