de6a1476ee2ff994c2e61518ee015bfecea7efae
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-21 02:14:05 +0100

    version `1.0.1`, change indent tabs to `2`

--- acava-common/src/main/java/gg/acai/acava/commons/chart/Charts.java
+++ acava-common/src/main/java/gg/acai/acava/commons/chart/Charts.java
@@ -13,42 +13,42 @@ import java.util.List;
  */
 public class Charts {
 
-    public static ChartBuilder builder() {
-        return new ChartBuilder();
-    }
+  public static ChartBuilder builder() {
+    return new ChartBuilder();
+  }
 
-    protected static JsonArray getColumnArray() {
-        JsonArray cols = new JsonArray();
-        JsonObject col1 = new JsonObject();
-        col1.addProperty("label", "X");
-        col1.addProperty("type", "string");
-        JsonObject col2 = new JsonObject();
-        col2.addProperty("label", "Y");
-        col2.addProperty("type", "number");
-        cols.add(col1);
-        cols.add(col2);
-        return cols;
-    }
+  protected static JsonArray getColumnArray() {
+    JsonArray cols = new JsonArray();
+    JsonObject col1 = new JsonObject();
+    col1.addProperty("label", "X");
+    col1.addProperty("type", "string");
+    JsonObject col2 = new JsonObject();
+    col2.addProperty("label", "Y");
+    col2.addProperty("type", "number");
+    cols.add(col1);
+    cols.add(col2);
+    return cols;
+  }
 
-    protected static JsonArray getRowArray(List<Integer> data) {
-        JsonArray rows = new JsonArray();
-        for (int i = 0; i < data.size(); i++) {
-            JsonArray row = new JsonArray();
-            row.add(new JsonPrimitive("Point " + i));
-            row.add(new JsonPrimitive(data.get(i)));
-            rows.add(row);
-        }
-        return rows;
+  protected static JsonArray getRowArray(List<Integer> data) {
+    JsonArray rows = new JsonArray();
+    for (int i = 0; i < data.size(); i++) {
+      JsonArray row = new JsonArray();
+      row.add(new JsonPrimitive("Point " + i));
+      row.add(new JsonPrimitive(data.get(i)));
+      rows.add(row);
     }
+    return rows;
+  }
 
-    protected static String getChartData(List<Integer> data) {
-        StringBuilder chartData = new StringBuilder();
-        for (int i = 0; i < data.size(); i++) {
-            chartData.append(data.get(i));
-            if (i != data.size() - 1)
-                chartData.append(",");
-        }
-        return chartData.toString();
+  protected static String getChartData(List<Integer> data) {
+    StringBuilder chartData = new StringBuilder();
+    for (int i = 0; i < data.size(); i++) {
+      chartData.append(data.get(i));
+      if (i != data.size() - 1)
+        chartData.append(",");
     }
+    return chartData.toString();
+  }
 
 }
