bc5619a90650983ad8aabf6be5ce75a7def38ed6
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-21 02:13:42 +0100

    version `1.0.1`, change indent tabs to `2`

--- acava-common/src/main/java/gg/acai/acava/commons/chart/AbstractChart.java
+++ acava-common/src/main/java/gg/acai/acava/commons/chart/AbstractChart.java
@@ -16,80 +16,81 @@ import java.util.List;
  */
 public abstract class AbstractChart implements Chart {
 
-    protected final List<Integer> nodes;
-    protected final int width;
-    protected final int height;
-    protected final String title;
-    protected final double maxX;
-    protected final double minX;
-    protected final double maxY;
-    protected final double minY;
+  protected final List<Integer> nodes;
+  protected final int width;
+  protected final int height;
+  protected final String title;
+  protected final double maxX;
+  protected final double minX;
+  protected final double maxY;
+  protected final double minY;
 
-    public AbstractChart(ChartBuilder builder) {
-        this.nodes = builder.nodes;
-        this.width = builder.width;
-        this.height = builder.height;
-        this.title = builder.title;
-        this.maxX = builder.maxX;
-        this.minX = builder.minX;
-        this.maxY = builder.maxY;
-        this.minY = builder.minY;
-    }
+  public AbstractChart(ChartBuilder builder) {
+    this.nodes = builder.nodes;
+    this.width = builder.width;
+    this.height = builder.height;
+    this.title = builder.title;
+    this.maxX = builder.maxX;
+    this.minX = builder.minX;
+    this.maxY = builder.maxY;
+    this.minY = builder.minY;
+  }
 
-    @Override
-    public Chart addNode(int data) {
-        nodes.add(data);
-        return this;
-    }
+  @Override
+  public Chart addNode(int data) {
+    nodes.add(data);
+    return this;
+  }
 
-    @Override
-    public Chart addNodes(List<Integer> data) {
-        nodes.addAll(data);
-        return this;
-    }
+  @Override
+  public Chart addNodes(List<Integer> data) {
+    nodes.addAll(data);
+    return this;
+  }
 
-    @Override
-    public Chart addNodes(int... data) {
-        for (int i : data) nodes.add(i);
-        return this;
-    }
+  @Override
+  public Chart addNodes(int... data) {
+    for (int i : data) nodes.add(i);
+    return this;
+  }
 
-    @Override
-    public List<Integer> nodes() {
-        return nodes;
-    }
+  @Override
+  public List<Integer> nodes() {
+    return nodes;
+  }
 
-    @Override
-    public String buildUrl() {
-        return "https://chart.googleapis.com/chart?" +
-                "cht=lc&" +
-                "chs="+ width + "x" + height +"&" +
-                "chd=t:" + Charts.getChartData(nodes) + "&" +
-                "chxt=x,y&" +
-                "chxr=0,"+ minX + "," + maxX + "|1," + minY + "," + maxY + "&" +
-                "chds="+ minY + "," + maxY + "&" +
-                "chtt=" + title.replaceAll(" ", "+");
-    }
+  @Override
+  public String buildUrl() {
+    return "https://chart.googleapis.com/chart?" +
+            "cht=lc&" +
+            "chs=" + width + "x" + height + "&" +
+            "chd=t:" + Charts.getChartData(nodes) + "&" +
+            "chxt=x,y&" +
+            "chxr=0," + minX + "," + maxX + "|1," + minY + "," + maxY + "&" +
+            "chds=" + minY + "," + maxY + "&" +
+            "chtt=" + title.replaceAll(" ", "+");
+  }
 
-    @Override @Deprecated
-    public void save() {
-        HttpURLConnection connection;
-        try {
-            connection = (HttpURLConnection) new URL(buildUrl()).openConnection();
-            connection.setRequestMethod("GET");
-            connection.setRequestProperty("User-Agent", "Mozilla/5.0");
-            connection.setDoOutput(true);
-            connection.connect();
-            InputStream inputStream = connection.getInputStream();
-            OutputStream outputStream = Files.newOutputStream(Paths.get("chart.png"));
-            byte[] buffer = new byte[1024];
-            int length;
-            while ((length = inputStream.read(buffer)) != -1)
-                outputStream.write(buffer, 0, length);
-            inputStream.close();
-            outputStream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+  @Override
+  @Deprecated
+  public void save() {
+    HttpURLConnection connection;
+    try {
+      connection = (HttpURLConnection) new URL(buildUrl()).openConnection();
+      connection.setRequestMethod("GET");
+      connection.setRequestProperty("User-Agent", "Mozilla/5.0");
+      connection.setDoOutput(true);
+      connection.connect();
+      InputStream inputStream = connection.getInputStream();
+      OutputStream outputStream = Files.newOutputStream(Paths.get("chart.png"));
+      byte[] buffer = new byte[1024];
+      int length;
+      while ((length = inputStream.read(buffer)) != -1)
+        outputStream.write(buffer, 0, length);
+      inputStream.close();
+      outputStream.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+  }
 }
--- acava-common/src/main/java/gg/acai/acava/commons/graph/AbstractGraph.java
+++ acava-common/src/main/java/gg/acai/acava/commons/graph/AbstractGraph.java
@@ -17,74 +17,82 @@ import java.util.List;
  */
 public class AbstractGraph<N extends Number> {
 
-    /** A GraphVisualizer object used for visualizing the graph. */
-    protected final GraphVisualizer<N> visualizer = new GraphVisualizer<>(this);
+  /**
+   * A GraphVisualizer object used for visualizing the graph.
+   */
+  protected final GraphVisualizer<N> visualizer = new GraphVisualizer<>(this);
 
-    /** A list of nodes in the graph. */
-    protected final List<N> nodes;
+  /**
+   * A list of nodes in the graph.
+   */
+  protected final List<N> nodes;
 
-    /** The maximum height of the graph. */
-    protected final int height;
+  /**
+   * The maximum height of the graph.
+   */
+  protected final int height;
 
-    /** The maximum number of nodes allowed in the graph. */
-    protected final int limiter;
+  /**
+   * The maximum number of nodes allowed in the graph.
+   */
+  protected final int limiter;
 
-    /**
-     * Constructs an AbstractGraph with an empty list of nodes.
-     *
-     * @param height the maximum height of the graph.
-     * @param limiter the maximum number of nodes allowed in the graph.
-     */
-    public AbstractGraph(int height, int limiter) {
-        this.nodes = new ArrayList<>();
-        this.height = height;
-        this.limiter = limiter;
-    }
+  /**
+   * Constructs an AbstractGraph with an empty list of nodes.
+   *
+   * @param height  the maximum height of the graph.
+   * @param limiter the maximum number of nodes allowed in the graph.
+   */
+  public AbstractGraph(int height, int limiter) {
+    this.nodes = new ArrayList<>();
+    this.height = height;
+    this.limiter = limiter;
+  }
 
-    /**
-     * Constructs an AbstractGraph with a fixed size queue of nodes.
-     *
-     * @param size the maximum number of nodes allowed in the queue.
-     * @param height the maximum height of the graph.
-     * @param limiter the maximum number of nodes allowed in the graph.
-     */
-    public AbstractGraph(int size, int height, int limiter) {
-        this.nodes = new FixedSizeQueue<>(size);
-        this.height = height;
-        this.limiter = limiter;
-    }
+  /**
+   * Constructs an AbstractGraph with a fixed size queue of nodes.
+   *
+   * @param size    the maximum number of nodes allowed in the queue.
+   * @param height  the maximum height of the graph.
+   * @param limiter the maximum number of nodes allowed in the graph.
+   */
+  public AbstractGraph(int size, int height, int limiter) {
+    this.nodes = new FixedSizeQueue<>(size);
+    this.height = height;
+    this.limiter = limiter;
+  }
 
-    /**
-     * Constructs an AbstractGraph with a given list of nodes.
-     *
-     * @param nodes a list of nodes to be added to the graph.
-     * @param height the maximum height of the graph.
-     * @param limiter the maximum number of nodes allowed in the graph.
-     */
-    public AbstractGraph(List<N> nodes, int height, int limiter) {
-        this.nodes = nodes;
-        this.height = height;
-        this.limiter = limiter;
-    }
+  /**
+   * Constructs an AbstractGraph with a given list of nodes.
+   *
+   * @param nodes   a list of nodes to be added to the graph.
+   * @param height  the maximum height of the graph.
+   * @param limiter the maximum number of nodes allowed in the graph.
+   */
+  public AbstractGraph(List<N> nodes, int height, int limiter) {
+    this.nodes = nodes;
+    this.height = height;
+    this.limiter = limiter;
+  }
 
-    /**
-     * Returns a list of all nodes in the graph.
-     *
-     * @return a list of nodes in the graph.
-     */
-    @Nonnull
-    public List<N> nodes() {
-        return nodes;
-    }
+  /**
+   * Returns a list of all nodes in the graph.
+   *
+   * @return a list of nodes in the graph.
+   */
+  @Nonnull
+  public List<N> nodes() {
+    return nodes;
+  }
 
-    /**
-     * Returns an immutable list of all nodes in the graph.
-     *
-     * @return an immutable list of nodes in the graph.
-     */
-    @Nonnull
-    public List<N> unmodifiable() {
-        return new ImmutableList<>(nodes);
-    }
+  /**
+   * Returns an immutable list of all nodes in the graph.
+   *
+   * @return an immutable list of nodes in the graph.
+   */
+  @Nonnull
+  public List<N> unmodifiable() {
+    return new ImmutableList<>(nodes);
+  }
 
 }
