b72c28c8099488ef50b8508f89839e939430d115
Clouke <76826179+Clouke@users.noreply.github.com>
2023-03-31 22:50:43 +0200

    create `CloseableCacheBootstrap` with `AutoCloseable`

---
+++ acava-common/src/main/java/gg/acai/acava/cache/CloseableCacheBootstrap.java
@@ -0,0 +1,8 @@
+package gg.acai.acava.cache;
+
+/**
+ * @author Clouke
+ * @since 31.03.2023 22:49
+ * © Acava - All Rights Reserved
+ */
+public interface CloseableCacheBootstrap extends CacheBootstrap, AutoCloseable {}
